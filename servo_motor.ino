000e05820ddeb0a441113399a5dc3c39c6bb48e9
Gopikaa01 <gopikaaashok1@gmail.com>
2024-06-25 12:20:40 +0530

    Update servo_motor.ino

--- servo_motor.ino
+++ servo_motor.ino
@@ -1,19 +1,1 @@
-#include<Servo.h>
-Servo myServo;
 
-const int servoPin=9;
-
-void setup() {
-  myServo.attach(servoPin);
-
-}
-void loop()
-{ myServo.write(0);
-delay(1000);
-
-myServo.write(90);
-delay(1000);
-
-myServo.write(180);
-delay(1000);
-}
